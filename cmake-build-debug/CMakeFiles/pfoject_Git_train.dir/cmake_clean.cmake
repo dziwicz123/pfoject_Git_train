71cee12a468a4e39d72e9fca4e77f6e2d8e50d7d
Michał Dziwisz <m.dziwisz@zsp10.elodz.edu.pl>
2022-01-10 14:52:43 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/pfoject_Git_train.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pfoject_Git_train.dir/main.cpp.obj"
+  "libpfoject_Git_train.dll.a"
+  "pfoject_Git_train.exe"
+  "pfoject_Git_train.exe.manifest"
+  "pfoject_Git_train.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pfoject_Git_train.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
